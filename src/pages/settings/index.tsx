06b20fd285d7f09b0cb6df18767e05b0d781c561
Kiran-Alex <kiranalexc@gmail.com>
2024-04-27 14:53:43 +0530

    minor fix

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -37,7 +37,7 @@ const index = () => {
                 <div style={{ height: "34em" }} className="w-2/5 mt-10 flex flex-col just\
                  ">
                     <div className="h-fit w-5/5"><h1 className="text-gray-500 text-xl font-bold">Profile</h1></div>
-                    <div className="flex-grow h-4/5">
+                    <div className="flex-grow h-5/5">
                         <div className="w-5/5 flex flex-row justify-center mt-6">
                             {profilepic.isFetched&& profilepic.data?.profilePicture!== undefined && <Image className="object-cover w-48 h-48 rounded-full" width={170} height={12} src={profilepic.data?.profilePicture} alt="" />}
                             {profilepic.isLoading && <div className="animate-pulse w-48 h-48 rounded-full bg-gray-300" ></div> }
@@ -51,11 +51,7 @@ const index = () => {
 
                         <input type="text" placeholder={userInfo.data?.info.mail ?? ""} disabled className="block  mt-2 w-full  dark:placeholder-gray-300 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-300  dark:text-gray-300 dark:focus:border-blue-300" />
                     </div>
-                    <div className="w-5/5 h-fit flex flex-row justify-end"><button className="px-3 py-1 text-sm tracking-wide flex flex-row font-thin text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                        save&nbsp; <svg className="w-4 h-5 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
-                        </svg>
-                    </button> </div>
+                   
                 </div>
             </div>
         </Layout>
